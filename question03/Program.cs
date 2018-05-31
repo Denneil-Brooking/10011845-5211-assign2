39b8d3df3c49a6663d416c5ce1bb9d03d7e92f84
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2018-05-31 16:41:22 +1200

    Q3 added stopwatch to time ticks

--- question03/Program.cs
+++ question03/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace question03
 {
@@ -7,13 +8,19 @@ namespace question03
     {
         static void Main(string[] args)
         {
-            string[] fruitsArray = new string[] {"Banana", "Grapefruit", "Tomato", "Oranges"};
 
-            //List<string> fruitsArray = new List<string> {"Banana", "Grapefruit", "Tomato", "Oranges"};
 
-            foreach(var fruit in fruitsArray) {
+            string[] fruitsArray = new string[] { "Banana", "Grapefruit", "Tomato", "Oranges"};
+
+            //List<string> fruitsArray = new List<string> { "Banana", "Grapefruit", "Tomato", "Oranges"};
+            
+            Stopwatch sw = Stopwatch.StartNew();
+            foreach (var fruit in fruitsArray)
+            {
                 Console.WriteLine(fruit);
             }
+            sw.Stop();
+            Console.WriteLine("Operation took {0}Ticks", sw.ElapsedTicks);
         }
     }
 }
