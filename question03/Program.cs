f4832be2ffe81694ce79a2948a520fe83a389311
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2018-05-31 16:50:45 +1200

    Q3 added more items

--- question03/Program.cs
+++ question03/Program.cs
@@ -10,9 +10,9 @@ namespace question03
         {
 
 
-            string[] fruitsArray = new string[] { "Banana", "Grapefruit", "Tomato", "Oranges"};
+            string[] fruitsArray = new string[] { "Banana", "Grapefruit", "Tomato", "Oranges", "Apple", "Cherry", "Mango", "Pear", "Apricot", "Plum", "Feijoa", "Watermelon", "Cranberry", "Grape", "Pomegranate", "Jujube", "Nectarine", "Raspberry", "Dragonfruit", "Kiwifruit", "Strawberry", "Olive", "Lemon", "Lychee", "Blackcurrant", "Passionfruit", "Guava", "Nance"};
 
-            //List<string> fruitsArray = new List<string> { "Banana", "Grapefruit", "Tomato", "Oranges"};
+            //List<string> fruitsArray = new List<string> { "Banana", "Grapefruit", "Tomato", "Oranges, "Apple", "Cherry", "Mango", "Pear", "Apricot", "Plum", "Feijoa", "Watermelon", "Cranberry", "Grape", "Pomegranate", "Jujube", "Nectarine", "Raspberry", "Dragonfruit", "Kiwifruit", "Strawberry", "Olive", "Lemon", "Lychee", "Blackcurrant", "Passionfruit", "Guava", "Nance""};
             
             Stopwatch sw = Stopwatch.StartNew();
             foreach (var fruit in fruitsArray)
